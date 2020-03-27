51d6129512aa9438e9ec9bc1b58c57f359481d79
nirc7 <i2cs2013@gmail.com>
2020-03-27 16:55:25 +0300

    debug

--- 06 24-02-2020 React Router/router-demo/src/Components/Header.jsx
+++ 06 24-02-2020 React Router/router-demo/src/Components/Header.jsx
@@ -14,10 +14,10 @@ const Header =
     <div >
         <div style={mystyle}> hello Ruppin!</div>
         <Link className="linkStyle" to="/"> home </Link> |
-         <Link className="linkStyle" to="/page2"> Page2 </Link> |
-         <Link className="linkStyle" to="/page3/7"> Page3 </Link> |
-         <Link className="linkStyle" to="/personsList"> Persons List </Link>
-         <Link className="linkStyle" to="/addPerson"> Add Person </Link>
+        <Link className="linkStyle" to="/page2"> Page2 </Link> |
+        <Link className="linkStyle" to="/page3/7"> Page3 </Link> |
+        <Link className="linkStyle" to="/personsList"> Persons List </Link>
+        <Link className="linkStyle" to="/addPerson"> Add Person </Link>
     </div>
 export default Header;
 
