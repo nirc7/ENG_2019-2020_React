bca1225babf4f9e6cc4092098030f3627ef637c9
nirc7 <i2cs2013@gmail.com>
2020-04-12 12:45:23 +0300

    fetch demo + login

---
+++ 08  22-04-2020 11 Fetch And jQueryAjax calls/WebApiStudentsLoginDemo/WebApiStudentsLoginDemo/Controllers/StudentController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using WebApiStudentsLoginDemo.Models;
+
+namespace WebApiStudentsLoginDemo.Controllers
+{
+    //[EnableCors( "*", "*", "*")]
+    public class StudentController : ApiController
+    {
+        //[EnableCors( "*", "*", "*")]
+        public IEnumerable<Student> Get()
+        {
+            return StudentsDB.GetAllStudents();
+        }
+
+        //opt1 - time efficient
+        //[DisableCors]
+        public Student Get(string email, string password)
+        {
+            return StudentsDB.GetStudentByEmailAndPassword(email, password);
+        }
+
+        //opt2 - less code
+        //public Student Get(string email, string password)
+        //{
+        //    return StudentsDB.GetAllStudents()
+        //        .SingleOrDefault(st => st.Email == email && st.Password == password);
+        //}
+
+        //[DisableCors]
+        public Student Post([FromBody] Student val)
+        {
+            return StudentsDB.GetStudentByEmailAndPassword(val.Email, val.Password);
+        }
+    }
+}
---
+++ 08  22-04-2020 11 Fetch And jQueryAjax calls/WebApiStudentsLoginDemo/WebApiStudentsLoginDemo/Models/StudentsDB.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace WebApiStudentsLoginDemo.Models
+{
+    public class StudentsDB
+    {
+        static string strCon = @"Data Source=E440\SQLEXPRESS;Initial Catalog=UsersDB;Integrated Security=True";
+        public static List<Student> GetAllStudents() 
+        {
+            List<Student> sl = new List<Student>();
+            SqlConnection con = new SqlConnection(strCon);
+            SqlCommand comm = new SqlCommand("SELECT * FROM StudentsTB", con);
+            comm.Connection.Open();
+            SqlDataReader reader = comm.ExecuteReader();
+            while (reader.Read())
+            {
+                Student s = new Student(
+                    (int)reader["ID"],
+                    (string)reader["Name"], (string)reader["Email"], (string)reader["Password"],
+                    (int)reader["Grade"]);
+                sl.Add(s);
+            }
+            comm.Connection.Close();
+            return sl;
+        }
+
+        public static Student GetStudentByEmailAndPassword(string email, string password)
+        {
+            Student s = null;
+            SqlConnection con = new SqlConnection(strCon);
+            SqlCommand comm = new SqlCommand(
+                $" SELECT * FROM StudentsTB " +
+                $" WHERE Email='{email}' AND Password='{password}'", con);
+            comm.Connection.Open();
+            SqlDataReader reader = comm.ExecuteReader();
+            if(reader.Read())
+            {
+                s = new Student(
+                    (int)reader["ID"],
+                    (string)reader["Name"], (string)reader["Email"], (string)reader["Password"],
+                    (int)reader["Grade"]);
+            }
+            comm.Connection.Close();
+            return s;
+        }
+
+    }
+}
